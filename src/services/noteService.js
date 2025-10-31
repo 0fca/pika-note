5292df96ffa29c69ad0a3560ea0a35983ef0d7ac
0fca <lukasbownik99@gmail.com>
2025-10-31 12:05:27 +0100

    Fix url

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -3,7 +3,7 @@ import UnauthorizedException from "../components/exceptions/UnauthorizedExceptio
 
 export default class NoteService {
     constructor() {
-        this.baseUrl = "https://localhost:8001";
+        this.baseUrl = "https://noteapi.lukas-bownik.net";
     }
 
     async readData(url = '', method = 'GET') {
